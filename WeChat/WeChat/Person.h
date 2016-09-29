faf1312d846e83ba26b3d555fcb874a8a018b40f
XMG <XMG@163.com>
2016-09-29 20:22:21 +0800

    添加Person类

---
+++ WeChat/WeChat/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  WeChat
+//
+//  Created by 李宇哲 on 2016/9/29.
+//  Copyright © 2016年 liyuzhe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ WeChat/WeChat/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  WeChat
+//
+//  Created by 李宇哲 on 2016/9/29.
+//  Copyright © 2016年 liyuzhe. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
