75e11d83291aeae0ce4255f385dba63998bb2100
XMG <XMG@163.com>
2016-09-29 20:20:29 +0800

    初始化项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by 李宇哲 on 2016/9/29.
+//  Copyright © 2016年 liyuzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChat/WeChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChat
+//
+//  Created by 李宇哲 on 2016/9/29.
+//  Copyright © 2016年 liyuzhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
